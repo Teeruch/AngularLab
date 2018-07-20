176a2cab7fc209cfe1d8385a9c99cc4556b6d9bf
devilcastx <devilcastx@gmail.com>
2018-07-20 11:45:48 +0700

    Observable testing

---
+++ src/app/products/products.ts
@@ -0,0 +1,8 @@
+export interface Products {
+    id          : number;
+    productId?  : number;
+    productCode : string;
+    productName : string;
+    price       : string;
+    rating      : number;
+}
--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,13 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Products } from '../products/products';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private SERVICE_URL = "http://localhost:3000/products"
+
+  constructor(private http : HttpClient) { }
 
   hello() {
     console.log('Hello Service')
+  }
+
+  getAll() :Observable<Products[]>{
+    return this.http.get<Products[]>(this.SERVICE_URL)
   }
 }
